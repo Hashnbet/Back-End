a64a1eecf20035741c9e54c394759af23d6e9cdb
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-06-17 00:15:55 -0400

    Schema_updated

--- data/migrations/20200615172501_bets.js
+++ data/migrations/20200615172501_bets.js
@@ -1,11 +1,37 @@
 
-exports.up = function(knex) {
-  return knex.schema.createTable('bet', tbl => {
-      tbl.increments();
-      tbl.string('bet title', 255),notNullable();
+exports.up = function(knex, Promise) {
+  return knex.schema
+  .createTable('bet', tbl => {
+      tbl.increments().primary();
+      tbl.string('bet_title', 255).notNullable();
+      tbl.integer('bet_amount').notNullable();
+      tbl.boolean('bet_likes');
+      tbl.string('bet_comments',255);
+      tbl.date('bet_ends');
   })
+  .createTable('user_bets', tbl=>{
+      tbl.integer('user_id')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+      tbl.integer('friend_id')
+      .unsigned().notNullable()
+      .references('id').inTable('friends')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+      tbl.integer('bet_id')
+      .unsigned()
+      .references('id')
+      .inTable('bet')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+    return knex.schema
+    .dropTableIfExists('user_bets')
+    .dropTableIfExists('bet');
 };
