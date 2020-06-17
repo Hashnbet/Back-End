77612ca2c17846018587f7f9a44740329f30c568
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-06-17 16:09:32 -0400

    Added Config and auth folder

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const cors = require('cors');
+const helmet = require('helmet');
+
+
 const server = express();
 
+server.use(cors());
+server.use(helmet());
 server.use(express.json())
+
+
+
 
 server.get('/', (req,res)=> {
     res.status(200).json({message: "Welcome To Hash-n-bet API!"})
