e54713ae14a1188ebea36f6d401b04b2777d2b9d
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-06-28 00:42:09 -0400

    Added a second bet into seed data

--- data/seeds/003-Bets.js
+++ data/seeds/003-Bets.js
@@ -6,6 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('bet_table').insert([
         {id: 1, user_1_id: 1, user_2_id: 2, bet_title: 'I was right', bet_amount: 2000, bet_created: Date.now(), bet_Expires: '18-06-12 10:34:09 PM', bet_type: true, bet_Accepted: false },
+        {id: 2, user_1_id: 3, user_2_id: 2, bet_title: 'I was not right', bet_amount: 2000, bet_created: Date.now(), bet_Expires: '18-06-12 10:34:09 PM', bet_type: true, bet_Accepted: true },
       ]);
     });
 };
