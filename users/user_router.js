efb9dd1a91c56a15467a1a25b414bb4f41a7e0d2
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-07-01 00:08:33 -0400

    Removed console.log in functional routes

--- users/user_router.js
+++ users/user_router.js
@@ -6,52 +6,49 @@ const router = express.Router();
 // GET /api/users/ - GET All USERS Endpoint✅
 router.get('/', (req, res) => {
     Users.find()
-    .then(user => {
-        res.status(200).json(user);
-    })
-    .catch(error => {
-        res.status(500).json({ message: 'No users found'}, error);
-    })
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'No users found' }, error);
+        })
 });
 
 // GET /api/users/:id - GET User by ID Endpoint ✅
 router.get('/:id', (req, res) => {
     Users.findById(req.params.id)
-    .then(id => {
-        id ? res.status(200).json(id) : res.status(404).json({ message: 'No user with that ID'});
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ message: 'Failed to get user' });
-      });
+        .then(id => {
+            id ? res.status(200).json(id) : res.status(404).json({ message: 'No user with that ID' });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Failed to get user' });
+        });
 })
 
 // GET /api/users/:id - GET Users followers Endpoint ✅
 router.get('/:id/followers', (req, res) => {
     const { id } = req.params;
-  Users.getFollowers(id) 
-   .then(followers => {
-       console.log(followers)
+    Users.getFollowers(id)
+        .then(followers => {
+            console.log(followers)
 
-  
-   res.status(200).json(followers);
-   }).catch(error =>{
-       res.status(500).json({message: 'Error: Failed to get User Followers'})
-   })
+
+            res.status(200).json(followers);
+        }).catch(error => {
+            res.status(500).json({ message: 'Error: Failed to get User Followers' })
+        })
 })
 
 // GET /api/users/:id - GET Users that you are following Endpoint ✅
 router.get('/:id/following', (req, res) => {
     const { id } = req.params;
-  Users.getFollowing(id) 
-   .then(following => {
-       console.log(following)
-
-  
-   res.status(200).json(following);
-   }).catch(error =>{
-       res.status(500).json({message: 'Error: Failed to get User Following'})
-   })
+    Users.getFollowing(id)
+        .then(following => {
+            res.status(200).json(following);
+        }).catch(error => {
+            res.status(500).json({ message: 'Error: Failed to get User Following' })
+        })
 })
 
 
@@ -68,7 +65,6 @@ router.put('/:id', (req, res) => {
         changes.hasOwnProperty('kudos_bankRoll') ||
         changes.hasOwnProperty('bet_type')
     ) {
-
         Users.findById(id)
             .then(user => {
                 user ? Users.update(changes, id).then(update => {
